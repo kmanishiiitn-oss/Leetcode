d5eb272a13a28d9455c6a0501d48c1c7af1773fd
kmanishiiitn-oss <kmanish.iiitn@gmail.com>
2026-05-29 11:19:31 +0530

    Longestsubstr without repeating chararcters

---
+++ Arrays/Longestsubstrwithoutrep.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_map<char, int> mp;
+        int maxi = 0;
+        int l = 0;
+        for (int r = 0; r < s.size(); r++) {
+            mp[s[r]]++;
+
+            while (mp[s[r]] > 1) {
+                mp[s[l]]--;
+                l++;
+            }
+            maxi = max(maxi, r - l + 1);
+        }
+        return maxi;
+    }
+};
